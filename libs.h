72ca84c11923d911a1e816c35f62e6c38c0f801d
Christo Paradis <christoparadis@protonmail.com>
2022-02-25 14:09:36 -0500

    Init

---
+++ libs.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+
+#include <glew.h>
+#include <glfw3.h>
+
+
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include<gtc\matrix_transform.hpp>
+#include<SOIL2.h>
